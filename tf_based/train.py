be3b3cfe86ef3db0de85101ff2e8afdb132b4b5a
Pham Thuc Hung <hungpt169@gmail.com>
2019-09-29 21:49:26 +0900

    update default path

--- tf_based/train.py
+++ tf_based/train.py
@@ -4,7 +4,7 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_path', default='../../data/processed data/', type=str)
-    parser.add_argument('--output_path', default='../output/', type=str)
+    parser.add_argument('--output_path', default='../../output/', type=str)
     parser.add_argument('--n_embedding', default=50, type=int)
     parser.add_argument('--n_sampled', default=6000, type=int)
     parser.add_argument('--epochs', default=35, type=int)
