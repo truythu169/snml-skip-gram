94db6276adf98694a63056d380c72ce0e5f963ad
Pham Thuc Hung <hungpt169@gmail.com>
2020-01-17 23:49:57 +0900

    train all

--- tf_based/train_all.py
+++ tf_based/train_all.py
@@ -1,12 +1,15 @@
 from tf_based.skip_gram import SkipGram
 
 if __name__ == "__main__":
-    input_path = '../data/text8/'
-    output_path = '../output/1/'
+    input_path = '../../data/text8/'
+    output_path = '../../output/text8/20200117/snml/1/train1/'
 
-    dimension_list = [25, 75, 125, 175, 225, 275]
+    dimension_list = [100, 50]
 
     for dimension in dimension_list:
-        skip_gram = SkipGram(input_path, output_path, n_embedding=dimension)
-        skip_gram.train(n_sampled=3000, epochs=31, batch_size=10000, print_step=1000)
+        skip_gram = SkipGram(input_path, output_path, n_embedding=dimension,
+                             n_sampled=3000, epochs=150, batch_size=1000,
+                             snml=False, snml_dir='')
+        skip_gram.train(print_step=0, stop_threshold=0.000001)
         skip_gram.export_embedding()
+        skip_gram.export_model()
