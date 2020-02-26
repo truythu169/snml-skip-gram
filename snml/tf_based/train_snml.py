32d25333ba6fa25b652a8f51ac59cbc96fe51997
Pham Thuc Hung <hungpt169@gmail.com>
2020-02-26 22:22:24 +0900

    fix bug

--- snml/tf_based/train_snml.py
+++ snml/tf_based/train_snml.py
@@ -8,9 +8,9 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', default='../../../output/text8/20191209/snml/1/50dim/', type=str)
-    parser.add_argument('--context_path', default='../../../data/text8/contexts/', type=str)
-    parser.add_argument('--snml_train_file', default='../../../data/text8/scope1.csv', type=str)
+    parser.add_argument('--model', default='../../../output/wiki/20200226/1/50dim/', type=str)
+    parser.add_argument('--context_path', default='../../../data/wiki/contexts/', type=str)
+    parser.add_argument('--snml_train_file', default='../../../data/wiki/scope1.csv', type=str)
     parser.add_argument('--scope', default=10000, type=int)
     parser.add_argument('--epochs', default=2, type=int)
     parser.add_argument('--learning_rate', default=0.0026, type=float)
