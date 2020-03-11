9f608a14712b8bfbedbc84866ee18673659ccf53
Pham Thuc Hung <hungpt169@gmail.com>
2020-03-11 16:39:18 +0900

    before negative sampling

--- snml/tf_based/cv_line.py
+++ snml/tf_based/cv_line.py
@@ -31,15 +31,17 @@ def get_loss_list_batch(m, d, batch_size=100):
 
 
 if __name__ == "__main__":
-    dims = [50, 100, 150, 170]
+    dims = [100, 110, 120, 130, 140, 150, 160, 170, 180]
     n_sample = 100000
     # read snml train file
     data = np.genfromtxt('../../../data/wiki/scope.csv', delimiter=',').astype(int)
 
     for dim in dims:
+        print(dim)
         # full data
         model = Model('../../../output/wiki/20200126/1/train1/{}dim/step-90/'.format(dim),
                       '../../../data/text8/contexts/', n_context_sample=3000, learning_rate=0.1)
 
-        loss_list = get_loss_list_batch(model, data[:n_sample])
+        loss_list = get_loss_list_batch(model, data[299999:n_sample + 299999])
+        # loss_list = get_loss_list_batch(model, data[:n_sample])
         save_pkl(loss_list, 'cv_lines/cv_{}_dim.pkl'.format(dim), local=True)
