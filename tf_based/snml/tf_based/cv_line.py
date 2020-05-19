45dce77b01e2f46f8389e0331a1e684c49dcf158
Pham Thuc Hung <hungpt169@gmail.com>
2020-05-19 21:52:06 +0900

    run cv line

--- tf_based/snml/tf_based/cv_line.py
+++ tf_based/snml/tf_based/cv_line.py
@@ -31,17 +31,18 @@ def get_loss_list_batch(m, d, batch_size=100):
 
 
 if __name__ == "__main__":
-    dims = [300]  # 50, 100, 110, 120, 130, 140, 150, 160, 170, 180, 200
-    n_sample = 100000
+    dims = [50, 100, 110, 120, 130, 140, 150, 160, 200, 300]
+
     # read snml train file
-    data = np.genfromtxt('../../../data/wiki/scope.csv', delimiter=',').astype(int)
+    data = np.genfromtxt('../../../../data/wiki/scope.csv', delimiter=',').astype(int)
+    n_sample = 3000000
 
     for dim in dims:
         print(dim)
         # full data
-        model = Model('../../../output/wiki/20200126/1/train1/{}dim/step-90/'.format(dim),
-                      '../../../data/text8/contexts/', n_context_sample=3000, learning_rate=0.1)
+        model = Model('../../../output/wiki/20200126/1/train2/{}dim/step-90/'.format(dim),
+                      '../../../data/wiki/contexts/', n_context_sample=3000, learning_rate=0.1)
 
         loss_list = get_loss_list_batch(model, data[299999:n_sample + 299999])
-        # loss_list = get_loss_list_batch(model, data[:n_sample])
-        save_pkl(loss_list, 'cv_lines/cv_{}_dim.pkl'.format(dim), local=True)
+        save_pkl(loss_list, 'C:\\Users/hungp/Downloads/information criteria on sg/wiki/20200126 snml/cv_{}_dim.pkl'.format(dim), local=True)
+        # save_pkl(loss_list, 'cv_lines/cv_{}_dim.pkl'.format(dim), local=True)
